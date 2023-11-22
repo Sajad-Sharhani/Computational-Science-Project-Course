b98893a00e34e6d5195215eb241021b6db52115c
Rabia Bashir <rabiabashir21@gmail.com>
2023-11-22 21:44:57 +0100

    Acceleration Calculations and Plot.

--- spikes_medfilt.py
+++ spikes_medfilt.py
@@ -1,16 +1,4 @@
-## ----------------------------------------------------------------------------
-# Copyright (C) 2023 Sajad Sharhani
 #
-# @file         spikes_medfilt.py
-# @author       Sajad Sharhani
-# @brief        Spike Removal in Time Series Data
-# @details      This script processes time series data from a CSV file, 
-#               identifies and removes spikes using a median filter, and 
-#               visualizes both the original and cleaned data for comparison.
-#               It is designed to handle individual tracking data and employs
-#               the median filter from scipy's signal processing module to 
-#               mitigate the effects of transient spikes in signal data.
-# -----------------------------------------------------------------------------
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -71,11 +59,22 @@ def calculate_velocity(data):
     # Return the modified DataFrame with the new 'Velocity' column
     return data
 
-def combined_plots(interval_data, fixed_signal, spikes, velocity_threshold):
+def calculate_acceleration(data):
+    # Step 1: Calculate absolute velocity difference
+    abs_velocity_diff = data['Velocity'].diff().abs()
+    # Step 2: Calculate acceleration using the absolute velocity difference
+    data['Acceleration'] = abs_velocity_diff / data['TimeDiff']
+    # Step 3: Fill NaN values with 0
+    data['Acceleration'] = data['Acceleration'].fillna(0)
+    # Step 4: Return the modified DataFrame with the 'Acceleration' column added
+    print(data)
+    return data
+
+def combined_plots(interval_data, fixed_signal, spikes, velocity_threshold, acc_threshold):
     # Set up a grid of plots with 2 rows and 3 columns
     # The first row for the original and cleaned signals, and velocity distribution
     # The second row for the scatter plots with original and cleaned data, and highlighted speeds
-    fig, axs = plt.subplots(2, 3, figsize=(18, 12))
+    fig, axs = plt.subplots(2, 4, figsize=(16, 8))
 
     # Original signal over time with spikes highlighted
     axs[0, 0].plot(interval_data['Timestamp'], interval_data['Y'], label='Original Signal')
@@ -97,6 +96,12 @@ def combined_plots(interval_data, fixed_signal, spikes, velocity_threshold):
     axs[0, 2].set_title('Velocity Distribution')
     axs[0, 2].set_xlabel('Velocity (units per second)')
     axs[0, 2].set_ylabel('Frequency')
+
+    # acceleration distribution
+    axs[0, 3].hist(interval_data['Acceleration'], bins=30, color='blue', alpha=0.7)
+    axs[0, 3].set_title('acc Distribution')
+    axs[0, 3].set_xlabel('acc (units per second)')
+    axs[0, 3].set_ylabel('Frequency')
 
     # Scatter plot with original data
     axs[1, 0].scatter(interval_data['X'], interval_data['Y'], label='Original Data', alpha=0.5)
@@ -123,12 +128,22 @@ def combined_plots(interval_data, fixed_signal, spikes, velocity_threshold):
     axs[1, 2].set_ylabel('Y Position')
     axs[1, 2].legend()
 
+    # Scatter plot with acceleration highlighted
+    normal_acc = interval_data['Acceleration'] <= acc_threshold
+    abnormal_acc = interval_data['Acceleration'] > acc_threshold
+    axs[1, 3].scatter(interval_data['X'][normal_acc], interval_data['Y'][normal_acc], label='Normal Acceleration', alpha=0.5)
+    axs[1, 3].scatter(interval_data['X'][abnormal_acc], interval_data['Y'][abnormal_acc], color='red', label='Abnormal Acceleration', alpha=0.5)
+    axs[1, 3].set_title('Scatter Plot with Acceleration Highlighted')
+    axs[1, 3].set_xlabel('X Position')
+    axs[1, 3].set_ylabel('Y Position')
+    axs[1, 3].legend()
+
     plt.tight_layout()
     plt.show()
 
 
 def main():
-    filename = 'FA_20191115T000000UTC.csv'
+    filename = '/home/rabia/Current/CSProj/FA_20191115T000000UTC.csv'
     individual_id = 2417246
     start_time = '2019-11-15 02:05:00'
     end_time = '2019-11-15 02:20:00'
@@ -138,18 +153,22 @@ def main():
     if data is not None:
         individual_data = get_individual(data, individual_id)
         interval_data = get_interval(individual_data, start_time, end_time)
-        interval_data_with_velocity = calculate_velocity(interval_data)
+        interval_data = calculate_velocity(interval_data)
+        interval_data = calculate_acceleration(interval_data)
         fixed_signal, spikes = remove_spikes_and_identify(interval_data['Y'], spike_threshold)
 
         # Ensure 'Velocity' column exists before trying to access it
-        if 'Velocity' in interval_data_with_velocity.columns:
-            velocity_threshold = np.percentile(interval_data_with_velocity['Velocity'], 95)
+        if 'Velocity' in interval_data.columns:
+            velocity_threshold = np.percentile(interval_data['Velocity'], 95)
             print(f"The calculated velocity threshold is: {velocity_threshold}")
-            # Use the combined plot function here
-            combined_plots(interval_data_with_velocity, fixed_signal, spikes, velocity_threshold)
-        else:
-            print("Velocity column has not been created.")
 
+        # Ensure 'Acceleration' column exists before trying to access it
+        if 'Acceleration' in interval_data.columns:
+            acceleration_threshold = np.percentile(interval_data['Acceleration'], 95)
+            print(f"The calculated acceleration threshold is: {acceleration_threshold}")
+
+        # Used the combined plot function here
+        combined_plots(interval_data, fixed_signal, spikes, velocity_threshold, acceleration_threshold)
 if __name__ == "__main__":
     main()
 
